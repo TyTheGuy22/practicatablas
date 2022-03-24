d2b6db6a44ceaff6a9148247c32b84e60f42cc81
TyTheGuy22 <raul_161200@hotmail.com>
2022-03-23 21:32:02 -0700

    delete buttom

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  function borrar(cod) {
+    const temp = articulos.filter((art)=>art.codigo !== cod);
+    setArticulos(temp)
+  }
+  const [articulos, setArticulos] = useState([])
+  useEffect(() => {
+    fetch('https://scratchya.com.ar/react/datos.php')
+      .then((response) => {
+        return response.json()
+      })
+      .then((articulos) => {
+        setArticulos(articulos)
+      })
+  }, [])
+  return (
+    <div className="App">
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+            <th>Borrar</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {articulos.map(art => {
+            return (
+              <tr key={art.codigo}>
+                <td>{art.codigo}</td>
+                <td>{art.descripcion}</td>
+                <td>{art.precio}</td>
+                <td><button onClick={() => borrar(art.codigo)}>Borrar</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
